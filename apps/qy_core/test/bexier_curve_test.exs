01858c9ca2ea57aae1cfccf8e6f0e93a15132d8d
RoastChestnut <chestnut.roasted@outlook.com>
2024-11-25 16:29:12 +0800

    Create test helper.

--- apps/qy_core/test/bexier_curve_test.exs
+++ apps/qy_core/test/bexier_curve_test.exs
@@ -1,10 +1,17 @@
+defmodule QyCore.BexierCurveTest.Helpers do
+  # 需要一个 helper 来判断曲线是否经过某个区间/点
+  # assert_passby / assert_include / assert_exclude
+  defmacro assert_passby(_curve, _target, _distance), do: nil
+  defmacro assert_include(_curve, _target, _distance), do: nil
+  defmacro assert_exclude(_curve, _target, _distance), do: nil
+end
+
 defmodule QyCore.BexierCurveTest do
   use ExUnit.Case
 
   # alias QyCore.Params.BezierCurve
+  # require QyCore.BexierCurveTest.Helpers
 
-  # 需要一个 helper 来判断曲线是否经过某个区间/点
-  # assert_passby / assert_include / assert_exclude
   describe "画线" do
     # ...
   end
