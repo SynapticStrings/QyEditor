9b4baac2525e44313a1056be97ac16ece1333bd8
RoastChestnut <chestnut.roasted@outlook.com>
2024-11-27 23:57:04 +0800

    Add doc.

--- apps/diff_singer/README.md
+++ apps/diff_singer/README.md
@@ -1,3 +1,15 @@
 # DiffSinger
 
 Elixir's DiffSinger port.
+
+## Feature
+
+* Automatic building model generation graph from models
+  - like ConfyUI or Simulink, but its based on name of model or mannual config
+* Complex scheduler make different strategies
+  - Real-time
+  - Efficient
+
+## Why Elixir?
+
+* bla bla
--- examples/load_model.exs
+++ examples/load_model.exs
@@ -9,5 +9,19 @@ defmodule Qixuan_V2_5_0 do
     author_link: "https://github.com/openvpi/DiffSinger"
   }
 
-  def import_meta(), @meta |> IO.inspect(); nil
+  def import_meta(), do: @meta |> IO.inspect(); nil
 end
+
+## Temporary
+model_root_path = "priv/Qixuan_v2.5.0_DiffSinger_OpenUtau"
+model_path = fn sub -> Path.join(model_root_path, sub) end
+# Variance Model
+# pitch_predict_path = model_path.("")
+_linguisitic_path = model_path.("dsvariance/0816_qixuan_multilingual_multivar.qixuan.linguistic.onnx")
+_variance_path = model_path.("dsvariance/0816_qixuan_multilingual_multivar.qixuan.variance.onnx")
+# Acostic Model
+acostic_model_path = model_path.("0816_qixuan_multilingual_acoustic.qixuan.onnx")
+# Vocoder
+_vocoder_path = model_path.("dsvocoder/nsf_hifigan_qixuan_004.onnx")
+
+acostic_model = Ortex.load(acostic_model_path) |> IO.inspect()
