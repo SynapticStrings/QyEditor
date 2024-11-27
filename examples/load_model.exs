c1536b450972bd11a5194f9cac0df1c5c8cd65ff
RoastChestnut <chestnut.roasted@outlook.com>
2024-11-27 15:19:20 +0800

    Add code in example.

---
+++ examples/load_model.exs
@@ -0,0 +1,13 @@
+defmodule Qixuan_V2_5_0 do
+  @doc """
+  Model info.
+  """
+  @meta %{
+    name: "绮萱",
+    version: "v2.5.0",
+    author: "DiffSinger Community",
+    author_link: "https://github.com/openvpi/DiffSinger"
+  }
+
+  def import_meta(), @meta |> IO.inspect(); nil
+end
