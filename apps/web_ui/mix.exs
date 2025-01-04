105c645b0ebe5f3e78873d5769d9465f33228193
awaChestnut <chestnut.roasted@outlook.com>
2025-01-04 17:05:20 +0800

    WebUI][dep] 升级 `phoenix_live_view` 依赖版本至 1.0.0

--- apps/web_ui/mix.exs
+++ apps/web_ui/mix.exs
@@ -39,8 +39,7 @@ defmodule WebUI.MixProject do
       {:phoenix, "~> 1.7.14"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      # TODO: 升级到 {:phoenix_live_view, "~> 1.0.0"}, （前提是更新到了）
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:phoenix_live_view, "~> 1.0.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
