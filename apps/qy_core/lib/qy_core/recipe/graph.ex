037a94c4db225e4ac6fc5e07c06bfdd08e531c3c
RoastChestnut <chestnut.roasted@outlook.com>
2025-02-15 16:47:27 +0800

    考虑了下游多个步骤可能会使用同一个来源的情况；将 Builder 与 Helper 与 Graph 分开

--- apps/qy_core/lib/qy_core/recipe/graph.ex
+++ apps/qy_core/lib/qy_core/recipe/graph.ex
@@ -14,7 +14,17 @@ defmodule QyCore.Recipe.Graph do
   def get_step_from_vertex(name, steps), do: Enum.find(steps, &(&1.name == name))
 
   @spec build_conn_from_steps([Step.t()]) :: Graph.t()
-  def build_conn_from_steps([%Step{}] = steps) do
+  defdelegate build_conn_from_steps(steps), to: Graph.Builder
+
+  @spec get_graph_from_struct(Graph.t(), :digraph.graph()) :: :digraph.graph()
+  defdelegate get_graph_from_struct(graph_dict, graph \\ :digraph.new([])), to: Graph.Builder
+end
+
+defmodule QyCore.Recipe.Graph.Builder do
+  alias QyCore.Recipe.{Step, Graph}
+
+  @spec build_conn_from_steps([Step.t()]) :: Graph.t()
+  def build_conn_from_steps(steps) do
     steps
     |> Enum.map(& &1.name_tuple)
     |> Enum.reduce(
@@ -37,17 +47,20 @@ defmodule QyCore.Recipe.Graph do
         {_, 0} ->
           {name, :input, Enum.map(as_from, &{name, &1})}
 
-        {1, _} ->
+        {_, 1} ->
           # 如果是边的话
-          {name, :edge, Enum.map(as_to, &{&1, Enum.at(as_from, 0)})}
+          {name, :edge, Enum.map(as_from, &{Enum.at(as_to, 0), &1})}
 
-        _ ->
-          {name, :error}
+        context ->
+          {name, :error, {context, as_from, as_to}}
       end
     end)
-    |> Enum.reduce(%{orphans: [], input_port: [], output_port: [], edge: []}, &update_format/2)
+    |> Enum.reduce(
+      %{orphans: [], input_port: [], output_port: [], edge: []},
+      &update_format/2
+    )
     |> Map.merge(%{vertex: Enum.map(steps, & &1.name)})
-    |> then(&struct!(__MODULE__, &1))
+    |> then(&struct!(Graph, &1))
   end
 
   defp get_steps_from_edge(edge_name, steps) do
@@ -83,8 +96,12 @@ defmodule QyCore.Recipe.Graph do
     %{items | edge: edges ++ items[:edge]}
   end
 
+  defp update_format({name, :error, context}, _items) do
+    raise "Catch an error buring #{name} with a number #{inspect(context)}"
+  end
+
   @spec get_graph_from_struct(Graph.t(), :digraph.graph()) :: :digraph.graph()
-  def get_graph_from_struct(%__MODULE__{} = graph_dict, graph \\ :digraph.new([])) do
+  def get_graph_from_struct(%Graph{} = graph_dict, graph \\ :digraph.new([])) do
     Enum.map(graph_dict.vertex, &:digraph.add_vertex(graph, &1))
     Enum.map(graph_dict.input_port, &:digraph.add_vertex(graph, &1))
     Enum.map(graph_dict.output_port, &:digraph.add_vertex(graph, &1))
@@ -92,4 +109,52 @@ defmodule QyCore.Recipe.Graph do
 
     graph
   end
+end
+
+defmodule QyCore.Recipe.Graph.Helper do
+  # DFS 环检测
+  # 如果是由 Step 构建起来的图的话，这个函数是不需要的
+  # 代码由 ChatGPT 生成
+  @spec has_cycle?(:digraph.graph()) :: boolean()
+  def has_cycle?(graph) do
+    # 节点的状态，分别为 :unvisited, :visiting, :visited
+    initial_state = Map.new(:digraph.vertices(graph), fn v -> {v, :unvisited} end)
+
+    Enum.any?(:digraph.vertices(graph), fn vertex ->
+      dfs(graph, vertex, initial_state)
+    end)
+  end
+
+  defp dfs(graph, vertex, state) do
+    case Map.get(state, vertex) do
+      # 如果已访问，直接返回
+      :visited ->
+        false
+
+      # 如果正在访问，说明找到环
+      :visiting ->
+        true
+
+      :unvisited ->
+        # 标记当前节点为正在访问
+        state = Map.put(state, vertex, :visiting)
+
+        # 获取当前节点的邻接节点
+        neighbors = :digraph.in_neighbours(graph, vertex)
+
+        # 深度优先遍历邻接节点
+        result =
+          Enum.any?(neighbors, fn neighbor ->
+            dfs(graph, neighbor, state)
+          end)
+
+        # 完成遍历，标记当前节点为已访问
+        _state = Map.put(state, vertex, :visited)
+
+        result
+    end
+  end
+
+  # 最大的度数
+  # 先不考虑边或节点的权重
 end
--- examples/qy_core/recipe/graph_example.exs
+++ examples/qy_core/recipe/graph_example.exs
@@ -4,36 +4,52 @@ alias QyCore.Recipe.{Step, Graph}
 step1 = %Step{
   name: :step1,
   name_tuple: {{:i1, :i2}, {:m1}},
-  init: &(&1),
+  init: & &1,
   call: fn {a, b}, _ -> {a + b} end
 }
 
 step2 = %Step{
   name: :step2,
   name_tuple: {{:i2}, {:m2}},
-  init: &(&1),
+  init: & &1,
   call: fn {a}, _ -> {a * 2} end
 }
 
 step3 = %Step{
   name: :step3,
   name_tuple: {{:m1, :m2}, {:o1}},
-  init: &(&1),
+  init: & &1,
   call: fn {a, b}, _ -> {a * b} end
 }
+
+defmodule Step4 do
+  def call({input_1, middle_1}, _opts) do
+    {input_1 + middle_1 * 2}
+  end
+
+  def inject() do
+    %Step{name: :step4, name_tuple: {{:i1, :m1}, {:o2}}, init: & &1, call: &call/2}
+  end
+end
 
 %{i1: 1, i2: 3, m1: nil, m2: nil, o1: nil}
 |> Step.exec(step1)
 |> Step.exec(step2)
 |> Step.exec(step3)
+|> Step.exec(Step4.inject())
 |> IO.inspect(label: :mannual_load)
 
 ### USING GRAPH TO CONNECT NODE
 
-g = [step1, step2, step3]
+g =
+  [step1, step2, step3, Step4.inject()]
   |> Graph.build_conn_from_steps()
-  |> IO.inspect(label: :graph_dict)
+  # |> IO.inspect(label: :graph_dict)
   |> Graph.get_graph_from_struct()
-  |> IO.inspect(label: :graph)
+  # |> IO.inspect(label: :graph)
 
-:digraph_utils.components(g) |> IO.inspect()
+Graph.Helper.has_cycle?(g) |> IO.inspect(label: :cycle?)
+
+:digraph_utils.topsort(g) |> IO.inspect(label: :topsort)
+
+:digraph_utils.components(g) |> IO.inspect(label: :components)
