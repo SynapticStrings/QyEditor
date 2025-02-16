a9ed7cb9cb885b1639ef6c5a361521031a9ec7ca
RoastChestnut <chestnut.roasted@outlook.com>
2025-02-16 14:51:43 +0800

    定义了 QyCore.Recipe.SequenceHelper 模块，其可以实现 Param 和 {inner, context} 的转化

--- apps/qy_core/lib/qy_core/param/extract.ex
+++
@@ -1,6 +0,0 @@
-defmodule QyCore.Param.Extract do
-  # 参数与序列本体的替换
-
-  # def inject/2 => :ok | {:error, reason}
-  # def extract/1 => {:ok, value} | {:error, reason}
-end
---
+++ apps/qy_core/lib/qy_core/param/sequence_helper.ex
@@ -0,0 +1,8 @@
+defmodule QyCore.Param.SequenceHelper do
+  # 1.
+  # %Param{} <--> seq: [], time_step: timestep, ...
+  # %{key => %Param{}} --> param and context
+
+  # 2.
+  # seq: [..bigdata..] <--> :ets.select
+end
