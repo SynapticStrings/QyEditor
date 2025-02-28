79c589b72f793d09c932a7c5c9a6ad0dbff5750d
awaChestnut <chestnut.roasted@outlook.com>
2025-02-28 11:30:34 +0800

    [QyCore][example] 重命名步骤，添加步骤映射和自动执行逻辑，优化图结构连接

--- apps/qy_core/examples/recipe/graph_example.exs
+++ apps/qy_core/examples/recipe/graph_example.exs
@@ -1,22 +1,21 @@
-# alias QyCore.Param
 alias QyCore.Recipe.{Step, Graph}
 
 step1 = %Step{
-  name: :step1,
+  name: :add,
   name_tuple: {{:i1, :i2}, {:m1}},
   prepare: & &1,
   call: fn {a, b}, _ -> {a + b} end
 }
 
 step2 = %Step{
-  name: :step2,
+  name: :copy,
   name_tuple: {{:i2}, {:m2}},
   prepare: & &1,
   call: fn {a}, _ -> {a * 2} end
 }
 
 step3 = %Step{
-  name: :step3,
+  name: :multi,
   name_tuple: {{:m1, :m2}, {:o1}},
   prepare: & &1,
   call: fn {a, b}, _ -> {a * b} end
@@ -28,33 +27,58 @@ defmodule Step4 do
   end
 
   def inject() do
-    %Step{name: :step4, name_tuple: {{:i1, :m1}, {:o2}}, prepare: & &1, call: &call/2}
+    %Step{name: :add_with_copied_value, name_tuple: {{:i1, :m1}, {:o2}}, prepare: & &1, call: &call/2}
   end
 end
 
-%{i1: 1, i2: 3, m1: nil, m2: nil, o1: nil}
-|> Step.exec(step1)
-|> Step.exec(step2)
-|> Step.exec(step3)
-|> Step.exec(Step4.inject())
-|> IO.inspect(label: :mannual_load)
+# TODO
+# Try to add a new step with multiple outputs
+
+step_mapper = %{
+  step1.name => step1,
+  step2.name => step2,
+  step3.name => step3,
+  Step4.inject().name => Step4.inject()
+}
+
+initial_prams = %{i1: 1, i2: 3, m1: nil, m2: nil, o1: nil}
+
+mannual_operate_result =
+  initial_prams
+  |> Step.exec(step1)
+  |> Step.exec(step2)
+  |> Step.exec(step3)
+  |> Step.exec(Step4.inject())
+  |> IO.inspect(label: :mannual_load)
 
 ### USING GRAPH TO CONNECT NODE
 
 s =
   [step1, step2, step3, Step4.inject()]
+  # Shuffle the list to avoid the order of the list
+  |> Enum.shuffle()
   |> Graph.build_conn_from_steps()
   |> IO.inspect(label: :graph_dict)
 
-g = s
+_g =
+  s
   |> Graph.get_graph_from_struct()
   |> IO.inspect(label: :graph)
-#
 
-# Graph.Helper.has_cycle?(g) |> IO.inspect(label: :cycle?)
+### GET ORDER THE EXECUTE
 
 {:ok, inner} = Graph.get_execution_order(s)
 
 IO.inspect(inner, label: :order)
 
-:digraph_utils.components(g) |> IO.inspect(label: :components)
+automatic_running_result =
+  Enum.reduce(inner, initial_prams,
+    fn step, acc ->
+      Step.exec(acc, step_mapper[step])
+    end
+  )
+  |> IO.inspect(label: :running)
+
+if automatic_running_result == mannual_operate_result do
+  IO.puts("Success!")
+end
