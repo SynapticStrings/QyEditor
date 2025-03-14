1effab0f225924507822ab8c5a03e4598c537d7b
RoastChestnut <chestnut.roasted@outlook.com>
2025-03-14 15:07:05 +0800

    [QyMusic][refrac] 简单重构了部分整理音符的逻辑

--- apps/qy_music/lib/qy_music/note/format.ex
+++ apps/qy_music/lib/qy_music/note/format.ex
@@ -1,103 +1,137 @@
 defmodule QyMusic.Note.Format do
   alias QyMusic.Note
 
-  # 这个函数可能需要添加 :perference 是 :ss/:ff 的情况
-  def format(note, opts \\ [])
-
-  def format({_, var, _} = note, opts) do
+  # TODO 实现完全 :perference 是 :ss/:ff 的情况
+  def format({_, var, _} = note, opts  \\ []) do
     prefer =
       opts
       |> Keyword.get(:preference, :natural)
 
-    # 将包含 ff/bb 的情况处理掉
-    res =
-      case var do
-        :ss -> do_format(note, :sharp)
-        :sharp -> do_format(note, :sharp)
-        :natural -> note
-        :flat -> do_format(note, :flat)
-        :ff -> do_format(note, :flat)
-      end
+    case var do
+      :ss -> do_format(note, :sharp)
+      :sharp -> do_format(note, :sharp)
+      :natural -> do_format(note, :natural)
+      :flat -> do_format(note, :flat)
+      :ff -> do_format(note, :flat)
+    end |> do_format(prefer)
+  end
 
-    case prefer do
-      :natural -> res
-      # 同音异名的转换
-      _ -> res |> do_format(prefer)
+  @valid_key [:a, :b, :c, :d, :e, :f, :g]
+
+  defp do_format(note, :natural), do: note
+
+  defp do_format({key, :ss, octave}, :sharp) when key in @valid_key do
+    case key do
+      :e -> {:f, :sharp, octave}
+      :b -> {:c, :sharp, octave + 1}
+      _ -> {key |> Note.Distance.up_opt(), :natural, octave}
     end
   end
 
-  defp do_format({key, :ss, octave}, :sharp) do
-    cond do
-      key in [:c, :d, :f, :g, :a] -> {key |> Note.Distance.up_opt(), :natural, octave}
-      key == :e -> {:f, :sharp, octave}
-      key == :b -> {:c, :sharp, octave + 1}
-      true -> :invalid_note
-    end
-  end
-
-  defp do_format({key, :ff, octave}, :flat) do
-    cond do
-      key in [:d, :e, :g, :a, :b] -> {key |> Note.Distance.down_opt(), :natural, octave}
-      key == :f -> {:e, :flat, octave}
-      key == :c -> {:b, :flat, octave - 1}
-      true -> :invalid_note
+  defp do_format({key, :ff, octave}, :flat) when key in @valid_key do
+    case key do
+      :f -> {:e, :flat, octave}
+      :c -> {:b, :flat, octave - 1}
+      _ -> {key |> Note.Distance.down_opt(), :natural, octave}
     end
   end
 
   # 用来处理一些诸如 #E bF 之类的情况
 
-  defp do_format({key, :sharp, octave} = note, :sharp) do
-    cond do
-      key in [:c, :d, :f, :g, :a] -> note
-      key == :e -> {:f, :natural, octave}
-      key == :b -> {:c, :natural, octave + 1}
+  defp do_format({key, :sharp, octave} = note, :sharp) when key in @valid_key do
+    case key do
+      :e -> {:f, :natural, octave}
+      :b -> {:c, :natural, octave + 1}
+      _ -> note
     end
   end
 
-  defp do_format({key, :flat, octave} = note, :flat) do
-    cond do
-      key in [:d, :e, :g, :a, :b] ->
-        note
-
-      key == :f ->
-        {:e, :natural, octave}
-
-      key == :c ->
-        {:b, :natural, octave - 1}
-        # true -> :invalid_note
+  defp do_format({key, :flat, octave} = note, :flat) when key in @valid_key do
+    case key do
+      :f -> {:e, :natural, octave}
+      :c -> {:b, :natural, octave - 1}
+      _ -> note
     end
   end
 
   # 主要用于同音异名
 
-  defp do_format({key, :flat, octave}, :sharp) do
-    cond do
-      key in [:d, :e, :g, :a, :b] -> {key |> Note.Distance.down_opt(), :sharp, octave}
-      key == :f -> {:e, :natural, octave}
-      key == :c -> {:b, :natural, octave - 1}
+  defp do_format({key, :flat, octave}, :sharp) when key in @valid_key do
+    case key do
+      :f -> {:e, :natural, octave}
+      :c -> {:b, :natural, octave - 1}
+      _ -> {key |> Note.Distance.down_opt(), :sharp, octave}
     end
   end
 
-  defp do_format({key, :sharp, octave}, :flat) do
-    cond do
-      key in [:c, :d, :e, :g, :a] ->
-        {key |> Note.Distance.up_opt(), :natural, octave}
-
-      key == :e ->
-        {:f, :natural, octave}
-
-      key == :b ->
-        {:c, :natural, octave + 1}
-        # true -> :invalid_note
+  defp do_format({key, :sharp, octave}, :flat) when key in @valid_key do
+    case key do
+      :e -> {:f, :natural, octave}
+      :b -> {:c, :natural, octave + 1}
+      _ -> {key |> Note.Distance.up_opt(), :natural, octave}
     end
   end
+
+  ## 用于很特殊的情况
 
   # sharp -> ss
+  defp do_format({key, :sharp, octave}, :ss) when key in @valid_key do
+    case key do
+      # #C -> ##B
+      :c -> {:b, :ss, octave - 1}
+      # #F -> ##E
+      :f -> {:e, :ss, octave}
+      _ -> {key, :sharp, octave}
+    end
+  end
+
   # flat -> ss
+  defp do_format({key, :flat, octave}, :ss) when key in @valid_key do
+    case key do
+      # bC -> ##A
+      :c -> {:a, :ss, octave - 1}
+      # bD -> ##B
+      :d -> {:b, :ss, octave - 1}
+      # bF -> ##D
+      :f -> {key |> Note.Distance.down_opt(), :ss, octave}
+      # bG -> ##E
+      :g -> {key |> Note.Distance.down_opt(), :ss, octave}
+      # bE -> #D
+      # bA -> #F
+      # bB -> #A
+      _ -> {key |> Note.Distance.down_opt(), :sharp, octave}
+    end
+  end
+
   # natural -> ss
+  defp do_format({key, :natural, octave}, :ss) when key in @valid_key do
+    case key do
+      # C -> ##B
+      :c -> {:b, :natural, octave - 1}
+      # F -> #E
+      :f -> {:e, :sharp, octave}
+      # D, E, G, A, B
+      _ -> {key |> Note.Distance.down_opt(), :natural, octave}
+    end
+  end
+
   # sharp -> ff
+  defp do_format({key, :sharp, _octave}, :ff) when key in @valid_key do
+    # case key do
+    #   :c
+    # end
+    raise "Not Implemented yet."
+  end
+
   # flat -> ff
+  defp do_format({key, :flat, _octave}, :ff) when key in @valid_key do
+    raise "Not Implemented yet."
+  end
+
   # natural -> ff
+  defp do_format({key, :natural, _octave}, :ff) when key in @valid_key do
+    raise "Not Implemented yet."
+  end
 
   defp do_format({_, :natural, _} = note, _), do: note
 end
