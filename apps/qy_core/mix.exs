de6e6b24bdc920fd2d363f4dec7961ba4720f26e
RoastedChestnut <chestnut.roasted@outlook.com>
2025-12-27 17:10:55 +0800

    fix(deps): update orchid to 0.4.1 and orchid_symbiont to 0.1.1

--- apps/qy_core/mix.exs
+++ apps/qy_core/mix.exs
@@ -26,8 +26,8 @@ defmodule QyCore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:orchid, "~> 0.4.0"},
-      {:orchid_symbiont, git: "https://github.com/SynapticStrings/OrchidSymbiont.git"},
+      {:orchid, "~> 0.4"},
+      {:orchid_symbiont, "~> 0.1"},
     ]
   end
 end
